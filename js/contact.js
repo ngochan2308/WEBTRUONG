f8f4326d2b211996ab3037d712475fa6f4e30d42
ngochan2308 <126307999+ngochan2308@users.noreply.github.com>
2023-03-24 19:28:40 +0700

    BUOI3-4

--- js/contact.js
+++ js/contact.js
@@ -1,62 +1,87 @@
 function SendInfo() {
     hoten = $("#txtHoTen").val();
     email = $("#txtEmail").val();
-    sdt = $("#txtPhone").val();
+    phone = $("#txtPhone").val();
     donvi = $("#txtDonVi").val();
+    // guiden =("#txtGuiDen").val();
     noidung = $("#txtNoiDung").val();
     capt = $("#txtCaptcha").val();
 
     //check hoten
     if (hoten == ""||hoten==null||hoten==undefined) {
-        alert("Vui long nhap ho ten")
+        alert("Vui lòng nhập họ tên! ")
         return;
     }
 
    
     //check mail
-    if (validateEmail(email) === false || validateEmail(email) === null) {
-        alert("Email khong hop le!")
-        return;
-    }
-
-    //check phone
-    if (validatePhone(phone) === false || (validatePhone(phone) === null)) {
-        alert("Dien thoai khong hop le!")
+    if (email == ""|email==null||email==undefined) {
+        alert("Vui lòng nhập email!")
         return;
     }
     
+    // if (validateEmail(email) === false || validateEmail(email) === null) {
+    //     alert("Email khong hop le!")
+    //     return;
+    // }
+
+    //check phone
+    if (phone == ""|phone==null||phone==undefined) {
+        alert("Vui lòng nhập số điện thoại!")
+        return;
+    }
+    // if (validatePhone(phone) === false || (validatePhone(phone) === null)) {
+    //     alert("Dien thoai khong hop le!")
+    //     return;
+    // }
+    
     //don vi
     if (donvi == ""||donvi==null||donvi==undefined) {
-        alert("Vui long nhap don vi")
+        alert("Vui lòng nhập đơn vị!")
         return;
     }
 
+        //gui den
+    // if (guiden == "" || guiden == null || guiden == undefined)
+    // {
+    //     alert("Vui lòng chọn nơi nhận!")
+    //     return;
+    // }
+
     //noi dung
     if (noidung == ""||noidung==null||noidung==undefined) {
-        alert("Vui long nhap noi dung")
+        alert("Vui lòng nhập nội dung!")
         return;
     }
 
     //captcha
     if (capt == ""||capt==null||capt==undefined) {
-        alert("Vui long nhap ma captcha")
+        alert("Vui lòng nhập mã captcha!")
         return;
     }
+    else
+    {
+      if(capt !="qGphJD")
+        {
+          alert("Mã captcha không chính xác!")
+          return;
+        }
+    }
 
+        //email
+    // const validateEmail = (email) => {
+    //     return String(email)
+    //         .toLowerCase()
+    //         .match(
+    //             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    //         );
+    // };
 
-    const validateEmail = (email) => {
-        return String(email)
-            .toLowerCase()
-            .match(
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            );
-    };
-
-    const validatePhone = (phone) => {
-        return String(phone)
-            .toLowerCase()
-            .match(
-                /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g
-            );
-    };
+    // const validatePhone = (phone) => {
+    //     return String(phone)
+    //         .toLowerCase()
+    //         .match(
+    //             /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g
+    //         );
+    // };
 }
